1f3d6133379177f171aed20e95f9a0ff77adfc6e
Holmes Mojica <holmesmojica.dev@gmail.com>
2026-04-22 03:52:36 -0500

    feat(logging-lib): add LogLevel support to LogEntry
    - Introduced LogLevel enum following industry standards
    - Integrated log severity into LogEntry model
    - Enabled structured log classification for filtering and monitoring
    - Established foundation for scalable and observable logging

--- Hm.Logging/Models/LogEntry.cs
+++ Hm.Logging/Models/LogEntry.cs
@@ -1,13 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Hm.Logging.Models
+﻿namespace Hm.Logging.Models
 {
     public class LogEntry
     {
-        [Required]
         public string Message { get; set; } = string.Empty;
-
-        [Required]
-        public DateTime Timestamp { get; set; }
+        public LogLevel Level { get; set; } = LogLevel.Information;
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
     }
 }
