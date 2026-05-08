14b428fb29d520e8aab6f0e6fc47d16f707550a4
holmesmojica <holmesmojica.dev@gmail.com>
2026-05-08 03:03:30 -0500

    feat(logging): add configurable logging options support

---
+++ Hm.Logging/Configuration/LoggingOptions.cs
@@ -0,0 +1,16 @@
+﻿using Hm.Logging.Enums;
+
+namespace Hm.Logging.Configuration
+{
+    /// <summary>
+    /// Represents configuration settings for the logging pipeline.
+    /// </summary>
+    public sealed class LoggingOptions
+    {
+        /// <summary>
+        /// Gets or sets the minimum log level allowed for processing.
+        /// Logs below this level will be ignored.
+        /// </summary>
+        public LogLevel MinimumLevel { get; set; } = LogLevel.Information;
+    }
+}
--- Hm.Logging/Extensions/ServiceCollectionExtensions.cs
+++ Hm.Logging/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Hm.Logging.Abstractions;
+using Hm.Logging.Configuration;
 using Hm.Logging.Core;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,9 +13,23 @@ namespace Hm.Logging.Extensions
         /// <summary>
         /// Registers the core logging services into the dependency injection container.
         /// </summary>
-        public static IServiceCollection AddHmLogging(this IServiceCollection services)
+        /// <param name="services">
+        /// The dependency injection service collection.
+        /// </param>
+        /// <param name="configure">
+        /// Optional configuration delegate used to customize
+        /// <see cref="LoggingOptions"/>.
+        /// </param>
+        /// <returns>
+        /// The updated <see cref="IServiceCollection"/> instance.
+        /// </returns>
+        public static IServiceCollection AddHmLogging(this IServiceCollection services, Action<LoggingOptions>? configure = null)
         {
+            LoggingOptions options = new();
+            configure?.Invoke(options);
+
             return services
+                .AddSingleton(options)
                 .AddScoped<ITraceContext, ActivityTraceContext>();
         }
     }
